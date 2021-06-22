53e787d977305b6dd190562656b5928a1120ba41
isabelasousa1708 <isa_ramos17@hotmail.com>
2021-06-22 16:21:39 -0300

    ultimas modificações

--- www/js/index.js
+++ www/js/index.js
@@ -55,7 +55,7 @@ function showPassword(){
 /**************CAPTURAR FOTO*****************************/
   var pictureSource;   // picture source
   var destinationType; // sets the format of returned value
-    
+  
     document.addEventListener("deviceready",onDeviceReady,false);
     function onDeviceReady() {
         pictureSource=navigator.camera.PictureSourceType;
@@ -63,7 +63,6 @@ function showPassword(){
         document.getElementById('cameraApp');
     }
     function onPhotoDataSuccess(imageData) {
-
       var smallImage = document.getElementById('smallImage');
       smallImage.style.display = 'block';
       smallImage.src = "data:image/jpeg;base64," + imageData;
